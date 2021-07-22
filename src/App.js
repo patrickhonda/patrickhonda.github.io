dfc06c8848e30d6788b0eda1e17e71dfd555121c
patrykmostowski <patryk0mostowski@gmail.com>
2021-07-23 00:24:20 +0200

    wow

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Code from './components/code'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="ptf">
+          portfolio coming soon
+        </div>
+        <div className="load">
+        loading<span>.</span><span>.</span><span>.</span>
+        </div>
+        <div className="code">
+          <Code />
+        </div>
+        <div className="code">
+          <Code />
+        </div>
       </header>
     </div>
   );
